03f814710297581f765132a5cf66cd372197edf7
David Teixeira <estag_int02@controlrpa.com.br>
2025-11-05 19:28:53 -0300

    fix: update CORS configuration and ensure proper exception handling in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  app.useGlobalFilters(new AllExceptionsFilter)
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://finansys-frontend.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new AllExceptionsFilter());
+  await app.listen(process.env.PORT ?? 3800);
 }
 bootstrap();
