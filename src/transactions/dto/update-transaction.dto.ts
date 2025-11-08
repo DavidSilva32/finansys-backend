86c7cf7c836861d71cd07d9e618ddf138e95df64
David Teixeira <estag_int02@controlrpa.com.br>
2025-11-08 03:09:01 -0300

    update dto

--- src/transactions/dto/update-transaction.dto.ts
+++ src/transactions/dto/update-transaction.dto.ts
@@ -1,4 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTransactionDto } from './create-transaction.dto';
+import { TransactionStatus, TransactionType } from '@prisma/client';
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsDateString,
+} from 'class-validator';
 
-export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
+export class UpdateTransactionDto {
+  @IsEnum(TransactionType)
+  @IsOptional()
+  type?: TransactionType;
+
+  @IsEnum(TransactionStatus)
+  @IsOptional()
+  status?: TransactionStatus;
+
+  @IsString()
+  @IsOptional()
+  category?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsOptional()
+  amount?: number;
+
+  @IsDateString()
+  @IsOptional()
+  date?: string;
+}
